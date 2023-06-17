e5692fceecbdb801b7bcaf27c24e69e5f004290b
Влад Язиков <vladzaraza@gmail.com>
2023-06-17 20:38:20 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,151 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Wrapper } from './Wrapper/Wrapper.styled';
 import { Section } from './Section/Section.styled';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-export class App extends React.Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+// export class App extends React.Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
 
-  componentDidMount() {
+//   componentDidMount() {
+//     const savedContacts = localStorage.getItem('contacts');
+
+//     if (savedContacts) {
+//       this.setState({ contacts: JSON.parse(savedContacts) });
+//     }
+//   }
+
+//   componentDidUpdate(prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   addContact = newContact => {
+//     const nameExist = this.state.contacts.find(
+//       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+//     );
+
+//     if (nameExist) {
+//       alert(`${newContact.name} is already in contacts.`);
+//     } else {
+//       this.setState(prevState => ({
+//         contacts: [...prevState.contacts, newContact],
+//       }));
+//     }
+//   };
+
+//   changeFilter = filter => {
+//     this.setState({ filter });
+//   };
+
+//   getFilteredContacts = () => {
+//     const { contacts, filter } = this.state;
+//     const lowercasedFilter = filter.toLowerCase();
+
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(lowercasedFilter)
+//     );
+//   };
+
+//   deleteContact = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+
+//   render() {
+//     const { filter } = this.state;
+//     const filteredContacts = this.getFilteredContacts();
+
+//     return (
+//       <Wrapper>
+//         <Section>
+//           <h1 className="form__title">Phonebook</h1>
+//           <ContactForm onSubmit={this.addContact} />
+//         </Section>
+//         {this.state.contacts.length > 0 && (
+//           <Section>
+//             <h2 className="contacts__list_title">Contacts</h2>
+//             <Filter value={filter} onChangeFilter={this.changeFilter} />
+//             <ContactList
+//               contacts={filteredContacts}
+//               onDeleteContact={this.deleteContact}
+//             />
+//           </Section>
+//         )}
+//       </Wrapper>
+//     );
+//   }
+// }
+
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
     const savedContacts = localStorage.getItem('contacts');
 
     if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
+      setContacts(JSON.parse(savedContacts));
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = newContact => {
-    const nameExist = this.state.contacts.find(
+  const addContact = newContact => {
+    const nameExist = contacts.find(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
 
     if (nameExist) {
       alert(`${newContact.name} is already in contacts.`);
     } else {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      const updatedContacts = [...contacts, newContact];
+      setContacts(updatedContacts);
+      localStorage.setItem('contacts', JSON.stringify(updatedContacts)); // Save to local storage here
     }
   };
 
-  changeFilter = filter => {
-    this.setState({ filter });
+  const changeFilter = filter => {
+    setFilter(filter);
   };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const getFilteredContacts = () => {
     const lowercasedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(lowercasedFilter)
     );
   };
+  const filteredContacts = getFilteredContacts();
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    const updatedContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts)); // Save to local storage here
   };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-
-    return (
-      <Wrapper>
+  return (
+    <Wrapper>
+      <Section>
+        <h1 className="form__title">Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
+      </Section>
+      {contacts.length > 0 && (
         <Section>
-          <h1 className="form__title">Phonebook</h1>
-          <ContactForm onSubmit={this.addContact} />
+          <h2 className="contacts__list_title">Contacts</h2>
+          <Filter value={filter} onChangeFilter={changeFilter} />
+          <ContactList
+            contacts={filteredContacts}
+            onDeleteContact={deleteContact}
+          />
         </Section>
-        {this.state.contacts.length > 0 && (
-          <Section>
-            <h2 className="contacts__list_title">Contacts</h2>
-            <Filter value={filter} onChangeFilter={this.changeFilter} />
-            <ContactList
-              contacts={filteredContacts}
-              onDeleteContact={this.deleteContact}
-            />
-          </Section>
-        )}
-      </Wrapper>
-    );
-  }
-}
+      )}
+    </Wrapper>
+  );
+};
